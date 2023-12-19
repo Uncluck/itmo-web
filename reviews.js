1e6cccac2f84e96e67877d4194cf983c23dbf6eb
Uncluck <dm.melnik2013@yandex.ru>
2023-12-19 10:44:48 +0300

    init lab 7

--- reviews.js
+++ reviews.js
@@ -1,25 +1,35 @@
-window.addEventListener('load', () => {
-    fetch(`https://jsonplaceholder.typicode.com/comments?postId_lte=2`)
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(item => {
-                const template = document.querySelector('template');
-                const clone = document.importNode(template.content, true);
-                const divElem = clone.querySelector('.div-review');
-                divElem.classList.add('div-review');
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
 
-                const divName = clone.querySelector('.div-review-name');
-                divName.innerHTML = `Name: ${item.name}`;
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
 
-                const divEmail = clone.querySelector('.div-review-email');
-                divEmail.innerHTML = `Email: ${item.email}`;
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
 
-                const divBody = clone.querySelector('.div-review-body');
-                divBody.innerHTML = `Body: ${item.body}`;
+fetch('https://jsonplaceholder.typicode.com/comments?postId_lte=2')
+    .then(response => response.json())
+    .then(data => {
+        const template = document.querySelector('template');
+        data.forEach((item) => {
+            const clone = template.content.cloneNode(true);
 
-                document.querySelector('.reviews').appendChild(clone);
-            })
-        })
-        .catch(error => console.log('error', error))
-})
+            const divName = clone.querySelector('.div-review-name');
+            divName.innerHTML = `<span class="bold-span">Name: <br> </span>${item.name}`;
 
+            const divEmail = clone.querySelector('.div-review-email');
+            divEmail.innerHTML = `<span class="bold-span">Email: <br> </span>${item.email}`;
+
+            const divBody = clone.querySelector('.div-review-body');
+            divBody.innerHTML = `<span class="bold-span">Body: <br> </span>${item.body}`;
+            swiper.appendSlide(clone);
+            swiper.update();
+        });
+    })
